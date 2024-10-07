904a63cdf98b7cde56f7706688d8c838d8457a26
Vishnu P <vinventor1999@gmail.com>
2024-10-07 15:13:41 +0530

    Latest commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import path from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  server: {
+    proxy: {
+      // Proxy requests to /api/jokes to your backend
+      '/api': {
+        target: 'https://server-app-six.vercel.app',
+        changeOrigin: true,
+        secure: false,  // If using HTTPS and self-signed certificates, set this to false
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
     },
   },
 });
