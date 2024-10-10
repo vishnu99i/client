d6f2f0543b2a85febcdff60e509d731fbac7c7ae
Vishnu P <vinventor1999@gmail.com>
2024-10-11 01:13:03 +0530

    New commit

--- src/components/auth/layout.jsx
+++ src/components/auth/layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import loginImg from "../../assets/login.jpg"
 
 function AuthLayout() {
   return (
     <div className="flex min-h-screen w-full">
-      <div className="hidden lg:flex items-center justify-center bg-[url('../../assets/login.jpg')] w-1/2 px-12">
+      <div className="hidden lg:flex items-center justify-center w-1/2 px-12" style={{backgroundImage: `url(${loginImg})`}} >
         <div className="max-w-md space-y-6 text-center text-primary-foreground">
           <h1 className="text-4xl font-extrabold tracking-tight text-slate-300">
             Welcome to Web Shopping
