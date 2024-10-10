45ff13160035aeb281af082cca17911c064b735f
Vishnu P <vinventor1999@gmail.com>
2024-10-10 18:44:51 +0530

    Image size managing

--- src/pages/shopping-view/home.jsx
+++ src/pages/shopping-view/home.jsx
@@ -122,7 +122,7 @@ function ShoppingHome() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="relative w-full h-[300px] md:h-[600px] overflow-hidden">
+      <div className="relative w-full h-[200px] md:h-[600px] overflow-hidden">
         {featureImageList && featureImageList.length > 0
           ? featureImageList.map((slide, index) => (
               <img
