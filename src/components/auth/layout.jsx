4d78eb10a2b0be071d3f2b66295ba9e4a9f85ca4
Vishnu P <vinventor1999@gmail.com>
2024-10-11 01:00:26 +0530

    Login page

--- src/components/auth/layout.jsx
+++ src/components/auth/layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function AuthLayout() {
   return (
     <div className="flex min-h-screen w-full">
-      <div className="hidden lg:flex items-center justify-center bg-[url('../src/assets/login.jpg')] w-1/2 px-12">
+      <div className="hidden lg:flex items-center justify-center bg-[url('../../assets/login.jpg')] w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center text-primary-foreground">
           <h1 className="text-4xl font-extrabold tracking-tight text-slate-300">
             Welcome to Web Shopping
