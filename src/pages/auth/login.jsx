41c9f956200c221b8ae08d97eb21920b9bea1bc3
Vishnu P <vinventor1999@gmail.com>
2024-10-11 13:17:55 +0530

    New commit

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -34,7 +34,7 @@ function AuthLogin() {
   }
 
   return (
-    <div className="mx-auto w-full max-w-md space-y-6">
+    <div className="mx-auto w-full max-w-md space-y-6 backdrop-blur-sm px-5">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground text-slate-100">
           Sign in to your account
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -38,7 +38,7 @@ function AuthRegister() {
   console.log(formData);
 
   return (
-    <div className="mx-auto w-full max-w-md space-y-6">
+    <div className="mx-auto w-full max-w-md space-y-6 backdrop-blur-sm px-5">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground text-slate-100">
           Create new account
