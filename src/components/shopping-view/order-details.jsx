a3f8f8c85205c893712db795172e550cce6a92c5
Vishnu P <vinventor1999@gmail.com>
2024-10-22 03:28:12 +0530

    order details

--- src/components/shopping-view/order-details.jsx
+++ src/components/shopping-view/order-details.jsx
@@ -8,34 +8,34 @@ function ShoppingOrderDetailsView({ orderDetails }) {
   const { user } = useSelector((state) => state.auth);
 
   return (
-    <DialogContent className="sm:max-w-[600px] bg-gradient-to-r from-black to-slate-900 h-[95vh] font-mono">
+    <DialogContent className="sm:max-w-[600px] bg-gradient-to-r from-black to-slate-900 h-[95vh] font-mono rounded-lg sm:3xl">
       <div className="grid gap-3">
-        <div className="grid gap-2">
-          <div className="flex mt-3 items-center justify-between">
-            <p className="font-medium text-slate-400">Order ID</p>
+        <div className="grid gap-1 sm:gap-2">
+          <div className="flex sm:mt-3 items-center justify-between bg-slate-900 rounded-lg pl-3">
+            <p className="font-medium text-slate-300">Order ID</p>
             <Label className="text-blue-600">{orderDetails?._id}</Label>
           </div>
-          <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium text-slate-400">Order Date</p>
+          <div className="flex mt-2 items-center justify-between bg-slate-900 rounded-lg pl-3">
+            <p className="font-medium text-slate-300">Order Date</p>
             <Label className="text-orange-600">{orderDetails?.orderDate.split("T")[0]}</Label>
           </div>
-          <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium text-slate-400">Order Price</p>
+          <div className="flex mt-2 items-center justify-between bg-slate-900 rounded-lg pl-3">
+            <p className="font-medium text-slate-300">Order Price</p>
             <Label className="text-green-600">₹{orderDetails?.totalAmount}</Label>
           </div>
-          <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium text-slate-400">Payment method</p>
+          <div className="flex mt-2 items-center justify-between bg-slate-900 rounded-lg pl-3">
+            <p className="font-medium text-slate-300">Payment method</p>
             <Label className="text-blue-600">{orderDetails?.paymentMethod}</Label>
           </div>
-          <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium text-slate-400">Payment Status</p>
+          <div className="flex mt-2 items-center justify-between bg-slate-900 rounded-lg pl-3">
+            <p className="font-medium text-slate-300">Payment Status</p>
             <Label className="text-orange-600">{orderDetails?.paymentStatus}</Label>
           </div>
-          <div className="flex mt-2 items-center justify-between">
-            <p className="font-medium text-blue-600">Order Status</p>
+          <div className="flex mt-2 items-center justify-between bg-slate-900 rounded-lg pl-3">
+            <p className="font-medium text-blue-500">Order Status</p>
             <Label>
               <Badge
-                className={`pt-2 pb-3 px-3 ${
+                className={`pt-2 pb-3 px-3 hover:scale-95 hover:bg-slate-950 ${
                   orderDetails?.orderStatus === "confirmed"
                     ? "bg-green-600"
                     : orderDetails?.orderStatus === "rejected"
@@ -52,10 +52,10 @@ function ShoppingOrderDetailsView({ orderDetails }) {
         <div className="grid gap-4 text-slate-300">
           <div className="grid gap-2">
             <div className="font-medium text-green-600">Order Details</div>
-            <ul className="grid gap-3">
+            <ul className="grid gap-3 bg-slate-900 rounded-lg pl-5">
               {orderDetails?.cartItems && orderDetails?.cartItems.length > 0
                 ? orderDetails?.cartItems.map((item) => (
-                    <li className="flex items-center justify-between">
+                    <li className="flex items-start sm:items-center flex-col sm:flex-row justify-between">
                       <span>Title: {item.title}</span>
                       <span>Quantity: {item.quantity}</span>
                       <span className="text-green-600">Price: ₹{item.price}</span>
@@ -68,7 +68,7 @@ function ShoppingOrderDetailsView({ orderDetails }) {
         <div className="grid gap-4">
           <div className="grid gap-2">
             <div className="font-medium text-green-600">Shipping Info</div>
-            <div className="grid gap-0.5 text-blue-600">
+            <div className="grid gap-0.5 text-slate-200 bg-slate-900 pl-3 rounded-lg">
               <span>{user.userName}</span>
               <span>{orderDetails?.addressInfo?.address}</span>
               <span>{orderDetails?.addressInfo?.city}</span>
