3c8499f37d8711ea1ce0d8b87d873cb35dc05c02
Vishnu P <vinventor1999@gmail.com>
2024-10-21 14:40:13 +0530

    User product details

--- src/components/shopping-view/product-details.jsx
+++ src/components/shopping-view/product-details.jsx
@@ -106,15 +106,15 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
 
   return (
     <Dialog open={open} onOpenChange={handleDialogClose}>
-      <DialogContent className="grid grid-cols-3 gap-x-2 gap-y-0 sm:gap-5 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw] font-mono bg-gradient-to-r from-black to-slate-700 h-[95vh] overflow-y-scroll mt-8 sm:mt-6 pl-2 sm:pl-5 pr-2 sm:pr-5">
-        <div className="relative rounded-lg my-auto">
+      <DialogContent className="grid grid-cols-3 gap-x-2 gap-y-0 sm:gap-5 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:max-w-[70vw] font-mono bg-gradient-to-r from-black to-slate-700 sm:h-[95vh] overflow-y-scroll mt-1 sm:mt-6 pl-2 sm:pl-5 pr-2 sm:pr-5">
+        <div className="relative rounded-lg">
           <img
             src={productDetails?.image}
             alt={productDetails?.title}
             className="aspect-square w-full object-contain h-[30vh]"
           />
         </div>
-        <div className="col-span-2">
+        <div className="col-span-2 mb-3">
           <div>
             <h1 className="text-xl sm:text-3xl font-extrabold text-slate-300">{productDetails?.title}</h1>
             <p className="text-muted-foreground text-lg sm:text-2xl sm:mb-3 sm:mt-2">
@@ -164,7 +164,7 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
           </div>
           </div>
 
-          {/* <Separator className="col-span-3"/> */}
+          <Separator className="col-span-3"/>
 
           <div className="col-span-3">
           <div className="max-h-[300px] overflow-auto">
