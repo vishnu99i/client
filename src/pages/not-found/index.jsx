57c6dbe241cc7d711b2a007274199f3c56674e8c
Vishnu P <vinventor1999@gmail.com>
2024-10-22 23:34:18 +0530

    Basic application

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,5 +1,5 @@
 function NotFound() {
-  return <div>page doesn't exists</div>;
+  return <div className="h-[100vh] w-[100vw] text-center pt-[30vh] text-3xl text-slate-300 bg-gradient-to-r  from-black to-slate-900">page doesn't exists</div>;
 }
 
 export default NotFound;
--- src/pages/unauth-page/index.jsx
+++ src/pages/unauth-page/index.jsx
@@ -1,5 +1,5 @@
 function UnauthPage() {
-  return <h1>You don't have access to view this page</h1>;
+  return <div className="h-[100vh] w-[100vw] text-center pt-[30vh] text-3xl text-slate-300 bg-gradient-to-r  from-black to-slate-900">You don't have access to view this page</div>;
 }
 
 export default UnauthPage;
